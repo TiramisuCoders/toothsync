cea6cf85673bac3ed4eaa61171fed628cf41235b
viiviys <vince.delossantos.cics@ust.edu.ph>
2025-07-04 18:14:16 +0800

    added landing page and log in pages
    may error lang about hydration issue but the code works and the interface will display. default na rin na page na mago-open is si landing kesa si dashboard (lore accurate yay)

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -6,6 +6,15 @@ import {
   type ThemeProviderProps,
 } from 'next-themes'
 
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children }: ThemeProviderProps) {
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="light"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </NextThemesProvider>
+  )
 }
